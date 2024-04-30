ca94c6cdc940c64c01ad6476a44caa411af3c2bd
Wesley <wesleyadwebster@gmail.com>
2024-04-30 14:42:06 -0500

    initial commit project pulled

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is from a tutorial of NextJS 14.
+
+The aim of this is to learn NextJS 14 features. For sake of simplicity using regular JavaScript. 
+I hope to make this a reference piece to myself or others or a sandbox to try new things. 
---
+++ app/news/page.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+
+const NewsPage = (props) => {
+    const articles = [
+        {
+            title: 'item1',
+            href: '1'
+        },
+        {
+            title: 'item2',
+            href: '2'
+        },
+        {
+            title: 'item3',
+            href: '3'
+        }
+    ];
+
+    return (
+        <>
+            <h1>News Items</h1>
+            <ul>
+                {articles.map((newsArticle) => {
+                    return (
+                        <li><Link href={newsArticle.href}>{newsArticle.title}</Link></li>
+                    )
+                })}
+            </ul>
+        </>
+    );
+};
+
+export default NewsPage;
