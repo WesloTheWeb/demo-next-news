660094f88d0e305da4f0f5a35333420a7d9b0fd4
Wesley <wesleyadwebster@gmail.com>
2024-08-14 23:12:29 -0500

    Added in error handling pages catch all for general and nested

--- app/news/[slug]/page.jsx
+++ app/news/[slug]/page.jsx
@@ -1,9 +1,14 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsPage({ params }) {
   // ? takes whatever the [] value is in folder:
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug);
+
+  if (!newsItem) {
+    return notFound();
+  };
 
   console.log('newsItem value', newsItem);
 
---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+
+    return (
+        <div id="error">
+            <h1>Not Found! :(</h1>
+            <p>Our code monkeys at HQ are working very hard to fix this.</p>
+        </div>
+    )
+};
